e3217a21de979e008e9ccd5b9bdd6ce37211b457
JoseJavier21 <92015037+JoseJavier21@users.noreply.github.com>
2024-02-06 20:43:37 +0100

    navegacion

--- match-fronted/src/app/components/login/login.component.ts
+++ match-fronted/src/app/components/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { Form, FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Usuario } from '../../model/Usuario';
 import { UsuarioService } from '../../services/usuario.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,46 +11,44 @@ import { UsuarioService } from '../../services/usuario.service';
 })
 export class LoginComponent {
 
-  form: FormGroup
-
+  form: FormGroup;
   user: Usuario = {
     id: 0,
     username: '',
     password: '',
+  };
+
+  constructor(private userService: UsuarioService, private formBuilder: FormBuilder, private router: Router) {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
-  
-
-constructor(private userService: UsuarioService, private formBuilder: FormBuilder){}
-
-
-
-comprobar(){
-
-  if(this.user.username != null && this.user.password != null) {
-    sessionStorage.setItem("username", this.user.username)
-    sessionStorage.setItem("password", this.user.password)
-    this.save()
-  }else{
-    alert("Introducir los datos correspondientes")
+  comprobar() {
+    if (this.form.valid) {
+      sessionStorage.setItem("username", this.user.username);
+      sessionStorage.setItem("password", this.user.password);
+      this.save();
+    } else {
+      alert("Introducir los datos correspondientes");
+    }
   }
 
-}
+  save() {
+    this.user.username = this.form.get('username')?.value;
+    this.user.password = this.form.get('password')?.value;
 
-  save(){
-    this.user.username = this.form.get('username')?.value
-    this.user.password = this.form.get('password')?.value
     this.userService.saveUser(this.user).subscribe({
-      next: res =>{
-        alert("Se ha registrado con exito")
-        console.log(res)
+      next: res => {
+        alert("Se ha registrado con éxito");
+        console.log(res);
+        this.router.navigate(['/home'])
       },
-      error: err =>{
-        alert("Ha ocurrido un error")
-        console.log("Ha ocurrido un error" + err)
+      error: err => {
+        alert("Ha ocurrido un error");
+        console.error("Ha ocurrido un error", err);
       }
-    })
-    
+    });
   }
-  
 }
