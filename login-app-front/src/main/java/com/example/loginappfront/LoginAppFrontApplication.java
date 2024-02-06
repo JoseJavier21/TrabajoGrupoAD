78f041dda0d6708212f6a4f689e684133905993c
JuLioVera <92015093+JuLiio77@users.noreply.github.com>
2024-02-06 18:07:58 +0100

    subir front

---
+++ login-app-front/src/main/java/com/example/loginappfront/LoginAppFrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginappfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAppFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAppFrontApplication.class, args);
+	}
+
+}
---
+++ login-app-front/src/test/java/com/example/loginappfront/LoginAppFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginappfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAppFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
