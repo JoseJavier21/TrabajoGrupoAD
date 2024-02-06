01233b4c107e7d939ba023021b5843a8359b4907
JoseJavier21 <92015037+JoseJavier21@users.noreply.github.com>
2024-02-06 20:32:10 +0100

    path y funciones login

--- match-fronted/src/app/components/login/login.component.ts
+++ match-fronted/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Form, FormBuilder, FormGroup } from '@angular/forms';
-import { Usuario } from 'src/app/model/Usuario';
+import { Usuario } from '../../model/Usuario';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -17,9 +18,37 @@ export class LoginComponent {
     password: '',
   }
 
+  
+
+constructor(private userService: UsuarioService, private formBuilder: FormBuilder){}
+
+
+
+comprobar(){
+
+  if(this.user.username != null && this.user.password != null) {
+    sessionStorage.setItem("username", this.user.username)
+    sessionStorage.setItem("password", this.user.password)
+    this.save()
+  }else{
+    alert("Introducir los datos correspondientes")
+  }
+
+}
 
   save(){
-
+    this.user.username = this.form.get('username')?.value
+    this.user.password = this.form.get('password')?.value
+    this.userService.saveUser(this.user).subscribe({
+      next: res =>{
+        alert("Se ha registrado con exito")
+        console.log(res)
+      },
+      error: err =>{
+        alert("Ha ocurrido un error")
+        console.log("Ha ocurrido un error" + err)
+      }
+    })
     
   }
   
